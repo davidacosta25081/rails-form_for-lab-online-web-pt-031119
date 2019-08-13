a801ae89312a71c966429b195582421abf4bccd1
David Acosta <davidacosta25081@yahoo.com>
2019-08-13 23:47:20 +0000

    Automatically backed up by Learn

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,75 @@
+class SchoolClassesController < ApplicationController
+
+
+  def show
+
+
+  end
+
+
+
+  def new
+
+  end
+
+
+
+  def edit
+
+
+  end
+
+
+
+
+
+  def create
+
+
+  end
+
+
+
+  def update
+
+
+  end
+
+
+
+  private
+
+
+
+  def student_params
+
+
+  end
+
+
+
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,51 @@
+class StudentsController < ApplicationController
+
+  def show
+
+
+  end
+
+
+
+  def new
+
+  end
+
+
+
+  def edit
+
+
+  end
+
+
+
+
+
+  def create
+
+
+  end
+
+
+
+  def update
+
+
+  end
+
+
+
+  private
+
+
+
+  def student_params
+
+
+  end
+
+
+
+
+end
